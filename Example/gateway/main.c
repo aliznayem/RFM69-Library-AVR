19dc2d9adb14c1fc62d28a39ae53fa9b03844a26
Ali Zulkar Nayem <nayem.cosmic@gmail.com>
2020-05-12 00:09:04 +0600

    Update main.c

--- Example/gateway/main.c
+++ Example/gateway/main.c
@@ -1,10 +1,6 @@
 /*
- *
  * Created: 4/17/2017 11:43:37 AM
- * Author : Zulkar Nayem
  */ 
-
-#define F_CPU 8000000
 
 #include <avr/io.h>
 #include <stdlib.h>
@@ -23,7 +19,7 @@
 int main(void)
 {
     // initialize RFM69
-    rfm69_init(433, NODEID,NETWORKID);
+    rfm69_init(433, NODEID, NETWORKID);
     setHighPower(1);   // if model number rfm69hw
     setPowerLevel(30); // 0-31; 5dBm to 20 dBm 
     encrypt(NULL);     // if set has to be 16 bytes. example: "1234567890123456"
@@ -38,8 +34,10 @@ int main(void)
         {
             _delay_ms(10);
             if(ACKRequested())
+	    {
 		char ack[0];
                 sendACK(ack, 0);
+	    }
             char stringData[16];
             for(uint8_t i=0;i<16;i++) // max 16 digit can be shown in this case
             {
