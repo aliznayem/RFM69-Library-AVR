535d858d1210629ed30a47f83b44fe7d01cbcfe3
Ali Zulkar Nayem <nayem.cosmic@gmail.com>
2020-05-12 00:06:45 +0600

    Update main.c

--- Example/node/main.c
+++ Example/node/main.c
@@ -1,11 +1,3 @@
-/*
- *
- * Created: 4/17/2017 12:05:29 PM
- * Author : Zulkar Nayem
- */ 
-
-#define F_CPU 8000000
-
 #include <avr/io.h>
 #include <stdlib.h>
 #include <util/delay.h>
@@ -18,11 +10,10 @@
 #define NODEID    3
 #define TONODEID  4
 
-
 int main(void)
 {
     // initialize RFM69
-    rfm69_init(433,NODEID,NETWORKID);
+    rfm69_init(433, NODEID, NETWORKID);
     setHighPower(1); // if model number rfm69hw
     setPowerLevel(30); // 0-31; 5dBm to 20 dBm
     encrypt(NULL); // if set it has to be 16 bytes. example: "1234567890123456"
