bba74b059c4cee1a22b64959e7ac86a8c1ac4919
Zulkar Nayem <zn@nextgendigitech.nl>
2020-05-28 13:56:02 +0600

    Added support for ATtiny84A

--- get_millis.c
+++ get_millis.c
@@ -23,6 +23,8 @@ void millis_init()
         TIMSK1 |= (1 << OCIE1A);
     #elif defined (__AVR_ATmega64__)
         TIMSK |= (1 << OCIE1A);
+	#elif defined (__AVR_ATtiny84A__)
+        TIMSK1 |= (1 << OCIE1A);
     #endif
 }
 
